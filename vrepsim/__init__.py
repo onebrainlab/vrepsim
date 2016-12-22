0d93a1ebdb1a4719e03f32ae29cd0af7c2b5cd06
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2016-12-22 14:30:04 +0100

    Add package initializer

---
+++ vrepsim/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""V-REPSim: V-REP simulator Python interface.
+
+V-REPSim provides a high-level interface based on the V-REP remote API for
+controlling V-REP simulations from an external Python program.
+"""
+
+__version__ = "0.1.0.dev1"
+__author__ = "Przemyslaw (Mack) Nowak"
