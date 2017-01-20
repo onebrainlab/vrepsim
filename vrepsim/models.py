43ef5b6bf0416a26557179374bb288d1ed698e4d
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2017-01-20 19:42:40 +0100

    Change interface to Pioneer P3-DX robot simulated in V-REP
    The interface to Pioneer P3-DX robot simulated in V-REP is changed to
    use the interface to an array of proximity sensors simulated in V-REP
    for handling ultrasonic sensors and the interface to an array of motors
    simulated in V-REP for handling wheel motors.

--- vrepsim/models.py
+++ vrepsim/models.py
@@ -11,7 +11,7 @@ following models:
 import vrep
 
 from vrepsim.exceptions import SimulationError
-from vrepsim.objects import SceneObject
+from vrepsim.objects import MotorArray, ProximitySensorArray, SceneObject
 
 
 class Model(SceneObject):
@@ -41,60 +41,5 @@ class PioneerBot(Model):
 
     def __init__(self, vrep_sim, name, us_sensor_names, motor_names):
         super(PioneerBot, self).__init__(vrep_sim, name)
-        if us_sensor_names:
-            self._us_sensor_names = us_sensor_names
-            self._us_sensor_handles = self._get_obj_handles(
-                self._us_sensor_names)
-            self._n_us_sensors = len(self._us_sensor_names)
-        else:
-            self._us_sensor_names = []
-            self._us_sensor_handles = []
-            self._n_us_sensors = 0
-        if motor_names:
-            self._motor_names = motor_names
-            self._motor_handles = self._get_obj_handles(self._motor_names)
-            self._n_motors = len(self._motor_names)
-        else:
-            self._motor_names = []
-            self._motor_handles = []
-            self._n_motors = 0
-
-    @property
-    def n_motors(self):
-        """Number of used motors."""
-        return self._n_motors
-
-    @property
-    def n_us_sensors(self):
-        """Number of used ultrasonic sensors."""
-        return self._n_us_sensors
-
-    def get_inv_us_sensor_data(self):
-        """Retrieve ultrasonic sensor data transformed such that smaller
-        values correspond to further distances.
-        """
-        sensor_data = []
-        for s, sensor_handle in enumerate(self._us_sensor_handles):
-            res, detect, point, _, _ = vrep.simxReadProximitySensor(
-                self._client_id, sensor_handle, vrep.simx_opmode_blocking)
-            if res == vrep.simx_return_ok:
-                if detect:
-                    sensor_data.append(1.0-point[2])  # data transformed
-                else:
-                    sensor_data.append(0.0)
-            elif res == vrep.simx_return_novalue_flag:
-                sensor_data.append(0.0)
-            else:
-                raise SimulationError("Could not retrieve data from "
-                                      "{}.".format(self._us_sensor_names[s]))
-        return sensor_data
-
-    def set_motor_speeds(self, motor_speeds):
-        """Set motor speeds."""
-        for m, motor_handle in enumerate(self._motor_handles):
-            res = vrep.simxSetJointTargetVelocity(
-                self._client_id, motor_handle, motor_speeds[m],
-                vrep.simx_opmode_blocking)
-            if res != vrep.simx_return_ok:
-                raise SimulationError("Could not update {} motor "
-                                      "speed.".format(self._motor_names[m]))
+        self.us_sensors = ProximitySensorArray(vrep_sim, us_sensor_names)
+        self.wheels = MotorArray(vrep_sim, motor_names)
