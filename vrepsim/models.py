e2dedd09725875012ca660bd254c14434a6701ef
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2017-01-20 19:50:52 +0100

    Change interface to a generic model simulated in V-REP
    The interface to a generic model simulated in V-REP is changed to remove
    the following functionality, which is no longer needed:
    
    - retrieving a handle to the specified component object;
    - retrieving handles to the specified component objects.

--- vrepsim/models.py
+++ vrepsim/models.py
@@ -8,9 +8,6 @@ following models:
 - Pioneer P3-DX robot.
 """
 
-import vrep
-
-from vrepsim.exceptions import SimulationError
 from vrepsim.objects import MotorArray, ProximitySensorArray, SceneObject
 
 
@@ -19,21 +16,6 @@ class Model(SceneObject):
 
     def __init__(self, vrep_sim, name):
         super(Model, self).__init__(vrep_sim, name)
-
-    def _get_obj_handle(self, obj_name):
-        """Retrieve handle to the specified component object."""
-        if not obj_name:
-            raise SimulationError("Could not retrieve handle to _Unnamed_.")
-        res, handle = vrep.simxGetObjectHandle(self._client_id, obj_name,
-                                               vrep.simx_opmode_blocking)
-        if res != vrep.simx_return_ok:
-            raise SimulationError(
-                "Could not retrieve handle to {}.".format(obj_name))
-        return handle
-
-    def _get_obj_handles(self, obj_names):
-        """Retrieve handles to the specified component objects."""
-        return [self._get_obj_handle(obj_name) for obj_name in obj_names]
 
 
 class PioneerBot(Model):
