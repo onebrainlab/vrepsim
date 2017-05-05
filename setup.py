b8307c14774262dba7b6a148f32237afd8b2327c
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2017-05-05 19:02:20 +0200

    Add package installer

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name="vrepsim",
+    version='0.1.0.dev1',
+    author="Przemyslaw (Mack) Nowak",
+    author_email="pnowak.mack@gmail.com",
+    description="High-level Python interface to V-REP simulator",
+    url="https://github.com/macknowak/vrepsim",
+    license="GNU General Public License v3 or later (GPLv3+)",
+    packages=['vrepsim'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later '
+        '(GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering'
+        ]
+    )
