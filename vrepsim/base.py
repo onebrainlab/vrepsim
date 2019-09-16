d79683ce5d8cff98301e7a30232d81caaa3fbba8
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2019-09-16 18:38:42 +0200

    Change argument for generic communicator with V-REP simulator
    The argument for generic communicator with V-REP simulator, which
    specifies an interface to V-REP remote API server, is changed such that
    if it is None, a default interface to V-REP remote API server is used.

--- vrepsim/base.py
+++ vrepsim/base.py
@@ -5,12 +5,17 @@ Assorted base data structures provide a generic communicator with V-REP
 simulator.
 """
 
+from vrepsim.simulator import get_default_simulator
+
 
 class Communicator(object):
     """Generic communicator with V-REP simulator."""
 
     def __init__(self, vrep_sim):
-        self._vrep_sim = vrep_sim
+        if vrep_sim is not None:
+            self._vrep_sim = vrep_sim
+        else:
+            self._vrep_sim = get_default_simulator(raise_on_none=True)
 
     @property
     def client_id(self):
