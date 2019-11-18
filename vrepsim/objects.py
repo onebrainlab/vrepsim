821abd9bf9d6949d0323001d4fbd1914165dffdf
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2019-11-18 14:45:05 +0100

    Add setting vision sensor near clipping plane
    Setting vision sensor near clipping plane is added to the functionality
    provided by the interface to vision sensor simulated in V-REP.

--- vrepsim/objects.py
+++ vrepsim/objects.py
@@ -616,6 +616,28 @@ class VisionSensor(SceneObject):
                                                   # decimal point)
         return clip_plane
 
+    def set_near_clip_plane(self, clip_plane):
+        """Set near clipping plane."""
+        if self._handle < 0:
+            if self._handle == MISSING_HANDLE:
+                raise RuntimeError(
+                    "Could not set near clipping plane of {}: missing name or "
+                    "handle.".format(self._name))
+            if self._handle == REMOVED_OBJ_HANDLE:
+                raise RuntimeError("Could not set near clipping plane of {}: "
+                                   "object removed.".format(self._name))
+        client_id = self.client_id
+        if client_id is None:
+            raise ConnectionError(
+                "Could not set near clipping plane of {}: not connected to "
+                "V-REP remote API server.".format(self._name))
+        res = vrep.simxSetObjectFloatParameter(
+            client_id, self._handle, vrep.sim_visionfloatparam_near_clipping,
+            clip_plane, vrep.simx_opmode_blocking)
+        if res != vrep.simx_return_ok:
+            raise ServerError("Could not set near clipping plane of {}."
+                              "".format(self._name))
+
 
 class MotorArray(object):
     """Interface to an array of motors simulated in V-REP."""
